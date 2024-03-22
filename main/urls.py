1947737b0c7ddd5eb34a4f71efaee2f72928e288
salohiddin674 <salohiddinzokirjonov819@gmail.com>
2024-03-22 16:06:59 +0500

    salohiddin

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,15 @@ from .views import *
 urlpatterns =[
     path('',index_view , name='index_url'),
     path('account/',account_view , name='account_url'),
+    path('about/',about_view , name='about_url'),
+    path('blog/',blog_view , name='blog_url'),
+    path('blog-single/',blog_single_view , name='blog_single_url'),
+    path('cart/',cart_view , name='cart_url'),
+    path('checkout/',checkout_view , name='checkout_url'),
+    path('contact/',contact_view , name='contact_url'),
+    path('product-single/',product_single , name='product_single_url'),
+    path('shop/',shop_view , name='shop_url'),
+    path('wishlist/',wishlist_view , name='wishlist_url'),
+
+
 ]
--- main/views.py
+++ main/views.py
@@ -8,3 +8,31 @@ def index_view(request):
 
 def account_view(request):
     return render(request, 'account.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+def blog_view(request):
+    return render(request,'blog.html')
+
+def blog_single_view(request):
+    return render(request, 'blog-single.html')
+
+def cart_view(request):
+    return render(request,'cart.html')
+
+def checkout_view(request):
+    return render(request,'checkout.html')
+
+def contact_view(request):
+    return render(request,'contact.html')
+
+def product_single(request):
+    return render(request,'product-single.html')
+
+def shop_view(request):
+    return render(request,'shop.html')
+
+def wishlist_view(request):
+    return render(request,'wishlist.html')
+
